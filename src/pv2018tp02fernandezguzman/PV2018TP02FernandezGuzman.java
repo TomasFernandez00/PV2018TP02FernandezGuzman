088a07935bdfbc6bc2fd4cb2a418414df899c366
TomasFernandez <laalaraca@gmail.com>
2018-04-08 02:26:26 -0300

    Implemente los puntos que habiamos hecho en clase que corresponden al punto 1 del trabajo practico. Tomas Fernandez

--- src/pv2018tp02fernandezguzman/PV2018TP02FernandezGuzman.java
+++ src/pv2018tp02fernandezguzman/PV2018TP02FernandezGuzman.java
@@ -1,21 +1,43 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Trabajo practico numero 2 programacion visual
+ * Escuela de Minas Dr. Horacio Carrillo
+ * Tomas Fernandez y Martin Guzman
+ * https://github.com/TomasFernandez00/PV2018TP02FernandezGuzman
  */
+
+//TODO agregar catch de errores
+
 package pv2018tp02fernandezguzman;
+
+import static java.lang.System.exit;
+import java.util.Scanner;
 
 /**
  *
- * @author Tomas Fernandez EdM
+ * @author Tomas Fernandez Martin Guzman EdM
  */
 public class PV2018TP02FernandezGuzman {
 
-	/**
-	 * @param args the command line arguments
-	 */
 	public static void main(String[] args) {
-		// TODO code application logic here
+		mostrarMenu();
+	}
+	public static void mostrarMenu() {
+		System.out.println("1: Punto 1");
+		System.out.println("2: Punto 2");
+		System.out.println("3: Punto 3");
+		System.out.println("4: Salir");
+		Scanner in = new Scanner(System.in);
+		int opt = in.nextInt();
+		switch(opt) {
+			case 1: Punto01 unPunto01 = new Punto01();
+				unPunto01.mostrarMenu();
+				main(null);
+			case 2:
+				main(null);
+			case 3:
+				main(null);
+			case 4: exit(0);
+		}
 	}
 	
 }
