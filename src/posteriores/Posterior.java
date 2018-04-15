020f85abc176503b172571f7797f245079524036
Elena <Elena@DESKTOP-0236ELV>
2018-04-15 19:50:15 -0300

    Punto 3 con error x3

--- src/posteriores/Posterior.java
+++ src/posteriores/Posterior.java
@@ -10,6 +10,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 /**
@@ -18,52 +19,9 @@ import java.util.Scanner;
  */
 public class Posterior {
 
-    private String dia;
-    private String mes;
-    private String año;
-
-    /**
-     * @return the dia
-     */
-    public String getDia() {
-        return dia;
-    }
-
-    /**
-     * @param dia the dia to set
-     */
-    public void setDia(String dia) {
-        this.dia = dia;
-    }
-
-    /**
-     * @return the mes
-     */
-    public String getMes() {
-        return mes;
-    }
-
-    /**
-     * @param mes the mes to set
-     */
-    public void setMes(String mes) {
-        this.mes = mes;
-    }
-
-    /**
-     * @return the año
-     */
-    public String getAño() {
-        return año;
-
-    }
-
-    /**
-     * @param año the año to set
-     */
-    public void setAño(String año) {
-        this.año = año;
-    }
+    private int dia;
+    private int mes;
+    private int año;
 
     public Posterior() {
     }
@@ -71,37 +29,59 @@ public class Posterior {
     public void ingresarFecha() {
         Scanner nu = new Scanner(System.in);
         System.out.println("Ingrese el dia: ");
-        setDia(nu.nextLine());
+        setDia(nu.nextInt());
         System.out.println("Ingrese el mes: ");
-        setMes(nu.nextLine());
+        setMes(nu.nextInt());
         System.out.println("Ingrese el año: ");
-        setAño(nu.nextLine());
+        setAño(nu.nextInt());
+        transFechaCal();
     }
 
-    public Date transFechaDate() {
-        String fecha;
-        Date fechaDevuelta = null;
-        fecha = getDia() + "-" + getMes() + "-" + getAño();
-        SimpleDateFormat formato = new SimpleDateFormat("EEEE MMMM d HH:mm:ss z yyyy");
-        try {
-            fechaDevuelta = formato.parse(fecha);
-        } catch (ParseException e) {
-            System.out.println("Error" + e);
-        }
-        return fechaDevuelta;
-    }
-
-    public Calendar transFechaCal(Date date) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
+    public Calendar transFechaCal() {
+        Calendar cal = new GregorianCalendar(2018, Calendar.FEBRUARY, 22);
+        cal.add(Calendar.DAY_OF_MONTH, 100);
         return cal;
     }
 
-    public Calendar sumarCien() {
-        Calendar uno;
-        ingresarFecha();
-        uno = transFechaCal(transFechaDate());
-        uno.add(Calendar.DAY_OF_MONTH, 100);
-        return uno;
+    /**
+     * @return the dia
+     */
+    public int getDia() {
+        return dia;
+    }
+
+    /**
+     * @param dia the dia to set
+     */
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    /**
+     * @return the mes
+     */
+    public int getMes() {
+        return mes;
+    }
+
+    /**
+     * @param mes the mes to set
+     */
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    /**
+     * @return the año
+     */
+    public int getAño() {
+        return año;
+    }
+
+    /**
+     * @param año the año to set
+     */
+    public void setAño(int año) {
+        this.año = año;
     }
 }
