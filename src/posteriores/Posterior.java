df92c2b5f5b5d7e225a66c569996fe619b0efae4
TomasFernandez <laalaraca@gmail.com>
2018-04-15 23:54:29 -0300

    El punto 3 ya anda masomenos, hay que arreglar el formato de salida y acomodarle el menu, para acceder al punto 3 es primero la opcion 2 y luego la 5

--- src/posteriores/Posterior.java
+++ src/posteriores/Posterior.java
@@ -10,7 +10,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 /**
@@ -19,9 +18,52 @@ import java.util.Scanner;
  */
 public class Posterior {
 
-    private int dia;
-    private int mes;
-    private int año;
+    private String dia;
+    private String mes;
+    private String año;
+
+    /**
+     * @return the dia
+     */
+    public String getDia() {
+        return dia;
+    }
+
+    /**
+     * @param dia the dia to set
+     */
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    /**
+     * @return the mes
+     */
+    public String getMes() {
+        return mes;
+    }
+
+    /**
+     * @param mes the mes to set
+     */
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    /**
+     * @return the año
+     */
+    public String getAño() {
+        return año;
+
+    }
+
+    /**
+     * @param año the año to set
+     */
+    public void setAño(String año) {
+        this.año = año;
+    }
 
     public Posterior() {
     }
@@ -29,60 +71,37 @@ public class Posterior {
     public void ingresarFecha() {
         Scanner nu = new Scanner(System.in);
         System.out.println("Ingrese el dia: ");
-        setDia(nu.nextInt());
+        setDia(nu.nextLine());
         System.out.println("Ingrese el mes: ");
-        setMes(nu.nextInt());
+        setMes(nu.nextLine());
         System.out.println("Ingrese el año: ");
-        setAño(nu.nextInt());
-        trans();
-    }
-    
-    public Calendar trans(){
-        Calendar cal = new GregorianCalendar(2018, Calendar.FEBRUARY, 22,23,11,44);
-       return cal;
-    }
-    
-    
-    
-        /**
-     * @return the dia
-     */
-    public int getDia() {
-        return dia;
+        setAño(nu.nextLine());
     }
 
-    /**
-     * @param dia the dia to set
-     */
-    public void setDia(int dia) {
-        this.dia = dia;
+    public Date transFechaDate() {
+        String fecha;
+        Date fechaDevuelta = null;
+        fecha = getDia() + "/" + getMes() + "/" + getAño();
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            fechaDevuelta = formato.parse(fecha);
+        } catch (ParseException e) {
+            System.out.println("Error" + e);
+        }
+        return fechaDevuelta;
     }
 
-    /**
-     * @return the mes
-     */
-    public int getMes() {
-        return mes;
+    public Calendar transFechaCal(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal;
     }
 
-    /**
-     * @param mes the mes to set
-     */
-    public void setMes(int mes) {
-        this.mes = mes;
-    }
-
-    /**
-     * @return the año
-     */
-    public int getAño() {
-        return año;
-    }
-
-    /**
-     * @param año the año to set
-     */
-    public void setAño(int año) {
-        this.año = año;
+    public Date sumarCien() {
+        Calendar uno;
+        ingresarFecha();
+        uno = transFechaCal(transFechaDate());
+        uno.add(Calendar.DAY_OF_YEAR, 100);
+        return uno.getTime();
     }
 }
